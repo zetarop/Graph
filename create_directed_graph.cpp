4d4473657d71edea79066d169fc28aab3dd62dd4
zetarop <arkupa199888@gmail.com>
2018-10-11 15:59:36 +0530

    Create create_directed_graph.cpp

---
+++ create_directed_graph.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define ve vector
+#define pb push_back
+using namespace std;
+
+int ver, edg;
+ve<ve<int> > adj;
+
+
+int main()
+{
+	cin>>ver>>edg;
+	adj.resize(ver);
+	for(int i=0;i<edg;i++)
+	{
+		int u,v;
+		cin>>u>>v;
+		u--,v--;
+		adj[u].pb(v);
+	}
+
+	cout<<"Graph has been created : <3"<<endl;
+
+	return 0;
+}
