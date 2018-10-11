53b61e4c0acf81a0aa92e971ace43e3389dc7d56
zetarop <arkupa199888@gmail.com>
2018-10-11 22:07:02 +0530

    Create max_num_of_edge_remain_DAG.cpp

---
+++ max_num_of_edge_remain_DAG.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define ve vector
+#define pb push_back
+#define inf 1000000007
+
+int main()
+{
+	int ver,edg;
+	cin>>ver>>edg;
+	cout<<ver*(ver-1)/2 - edges<<endl;
+	return 0;
+}
