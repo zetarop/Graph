db1f5d778207149a0f8af430d463c2f19612a24b
zetarop <arkupa199888@gmail.com>
2018-10-12 14:27:58 +0530

    Create maximum_dist_bet_two_ver_back.cpp

---
+++ maximum_dist_bet_two_ver_back.cpp
@@ -0,0 +1,54 @@
+/*
+  This backtracking algo works for directed and undirected graph.
+*/
+
+#include<bits/stdc++.h>
+#define ve vector
+#define pb push_back
+#define inf 1000000007
+
+int n;
+int m;
+int ans=0;
+ve<ve<pair<int,int> > > adj;
+ve<int> mark;
+
+
+void dfs(int u, int v, int sum)
+{
+	mark[u]=1;
+	//cout<<u+1<<" "<<sum<<endl;
+	for(auto a:adj[u])
+	{
+		if(a.first == v)
+		{
+			ans = max(ans, sum + a.second);
+		}
+		else if(!mark[a.first])
+			dfs(a.first, v, sum+a.second);
+	}
+	mark[u]=0;
+}
+
+
+int main()
+{
+	cin>>n>>m;
+	adj.resize(n);
+	mark.resize(n,0);
+	for(int i=0;i<m;i++)
+	{
+		int u,v,w;
+		cin>>u>>v>>w;
+		u--,v--;
+		adj[u].pb({v,w});
+		adj[v].pb({u,w});
+	}
+
+	int u,v;
+	cin>>u>>v;
+	u--,v--;
+	dfs(u,v,0);
+	cout<<ans<<endl;
+	return 0;
+}
